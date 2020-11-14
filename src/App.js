a1ae022c5395f1a5e56170fc72d844572658f588
Diogo Candido <diogocandido.s@gmail.com>
2020-11-14 10:02:30 -0800

    fixed names for components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header.js';
+import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Wrapper from './components/Wrapper';
